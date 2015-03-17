c0a6beeaa20ae1828cf5b4a6d72ca67197c40aa5
rogerhyam <r.hyam@rbge.org.uk>
2015-03-17 11:28:28 +0000

    build dev

--- update_core.sh
+++ update_core.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This script uses rsync to keep the core code of a audiowand tour up to date with a clone from github.
 
-rsync -avh --exclude 'data/*' audiowand/* www
+rsync -avh --exclude-from 'audiowand/exclude_from_build.txt' audiowand/* www
