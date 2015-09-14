893336ffa533cf87df34e98e8311c8e3f6e9f69e
rogerhyam <r.hyam@rbge.org.uk>
2015-09-14 17:01:27 +0100

    added splash for android

--- tools/generate_icons.sh
+++ tools/generate_icons.sh
@@ -4,6 +4,7 @@ mkdir ../../res
 
 # Android ICONS
 mkdir ../../res/android
+: <<'END'
 sips -z 36 36 "../../www/data/images/icon.png"  --out "../../res/android/icon_ldpi.png" 
 sips -z 48 48 "../../www/data/images/icon.png"  --out "../../res/android/icon_mdpi.png"
 sips -z 64 64 "../../www/data/images/icon.png"  --out "../../res/android/icon_tvdpi.png"
@@ -13,6 +14,31 @@ sips -z 144 144 "../../www/data/images/icon.png"  --out "../../res/android/icon_
 sips -z 192 192 "../../www/data/images/icon.png"  --out "../../res/android/icon_xxxhdpi.png"
 sips -z 500 500 "../../www/data/images/icon.png"  --out "../../res/android/icon_app_store.png"
 
+
+# Android splash
+sips -z 320 320 --setProperty format png "../../www/data/images/splash.jpg"  --out "../../res/android/splash_ldpi.png"
+sips -c 320 200 "../../res/android/splash_ldpi.png"  --out "../../res/android/splash_ldpi_portrait.png"
+sips -c 200 320 "../../res/android/splash_ldpi.png"  --out "../../res/android/splash_ldpi_landscape.png"
+rm "../../res/android/splash_ldpi.png"
+
+sips -z 480 480 --setProperty format png "../../www/data/images/splash.jpg"  --out "../../res/android/splash_mdpi.png"
+sips -c 480 320 "../../res/android/splash_mdpi.png"  --out "../../res/android/splash_mdpi_portrait.png"
+sips -c 320 480 "../../res/android/splash_mdpi.png"  --out "../../res/android/splash_mdpi_landscape.png"
+rm "../../res/android/splash_mdpi.png"
+
+sips -z 800 800 --setProperty format png "../../www/data/images/splash.jpg"  --out "../../res/android/splash_hdpi.png"
+sips -c 800 480 "../../res/android/splash_hdpi.png"  --out "../../res/android/splash_hdpi_portrait.png"
+sips -c 480 800 "../../res/android/splash_hdpi.png"  --out "../../res/android/splash_hdpi_landscape.png"
+rm "../../res/android/splash_hdpi.png"
+
+END
+
+sips -z 1280 1280 --setProperty format png "../../www/data/images/splash.jpg"  --out "../../res/android/splash_xhdpi.png"
+sips -c 1280 720 "../../res/android/splash_xhdpi.png"  --out "../../res/android/splash_xhdpi_portrait.png"
+sips -c 720 1280 "../../res/android/splash_xhdpi.png"  --out "../../res/android/splash_xhdpi_landscape.png"
+rm "../../res/android/splash_xhdpi.png"
+
+: <<'END'
 
 # iOS ICONS
 mkdir ../../res/ios
@@ -30,6 +56,7 @@ sips -z 58 58 "../../www/data/images/icon.png"  --out "../../res/ios/icon-small@
 sips -z 50 50 "../../www/data/images/icon.png"  --out "../../res/ios/icon-50.png"
 sips -z 100 100 "../../www/data/images/icon.png"  --out "../../res/ios/icon-50@2x.png"
 
+END
 
 
 
