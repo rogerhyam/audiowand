6177f03610fe805d8a7f071e6168fd971f5b264c
rogerhyam <r.hyam@rbge.org.uk>
2015-09-14 17:02:56 +0100

    fixed it this time

--- tools/generate_icons.sh
+++ tools/generate_icons.sh
@@ -4,7 +4,7 @@ mkdir ../../res
 
 # Android ICONS
 mkdir ../../res/android
-: <<'END'
+
 sips -z 36 36 "../../www/data/images/icon.png"  --out "../../res/android/icon_ldpi.png" 
 sips -z 48 48 "../../www/data/images/icon.png"  --out "../../res/android/icon_mdpi.png"
 sips -z 64 64 "../../www/data/images/icon.png"  --out "../../res/android/icon_tvdpi.png"
@@ -13,7 +13,6 @@ sips -z 96 96 "../../www/data/images/icon.png"  --out "../../res/android/icon_xh
 sips -z 144 144 "../../www/data/images/icon.png"  --out "../../res/android/icon_xxhdpi.png"
 sips -z 192 192 "../../www/data/images/icon.png"  --out "../../res/android/icon_xxxhdpi.png"
 sips -z 500 500 "../../www/data/images/icon.png"  --out "../../res/android/icon_app_store.png"
-
 
 # Android splash
 sips -z 320 320 --setProperty format png "../../www/data/images/splash.jpg"  --out "../../res/android/splash_ldpi.png"
@@ -31,14 +30,10 @@ sips -c 800 480 "../../res/android/splash_hdpi.png"  --out "../../res/android/sp
 sips -c 480 800 "../../res/android/splash_hdpi.png"  --out "../../res/android/splash_hdpi_landscape.png"
 rm "../../res/android/splash_hdpi.png"
 
-END
-
 sips -z 1280 1280 --setProperty format png "../../www/data/images/splash.jpg"  --out "../../res/android/splash_xhdpi.png"
 sips -c 1280 720 "../../res/android/splash_xhdpi.png"  --out "../../res/android/splash_xhdpi_portrait.png"
 sips -c 720 1280 "../../res/android/splash_xhdpi.png"  --out "../../res/android/splash_xhdpi_landscape.png"
 rm "../../res/android/splash_xhdpi.png"
-
-: <<'END'
 
 # iOS ICONS
 mkdir ../../res/ios
@@ -56,7 +51,7 @@ sips -z 58 58 "../../www/data/images/icon.png"  --out "../../res/ios/icon-small@
 sips -z 50 50 "../../www/data/images/icon.png"  --out "../../res/ios/icon-50.png"
 sips -z 100 100 "../../www/data/images/icon.png"  --out "../../res/ios/icon-50@2x.png"
 
-END
+
 
 
 
